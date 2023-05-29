3c8ead319ee53fde8b7ff22104e3e492f09314b3
Jan Erik Jakstein <janerik.jakstein@gmail.com>
2023-05-29 23:01:08 +0300

    rising edge input and input dictionary

--- input.py
+++ input.py
@@ -4,13 +4,18 @@
 import pygame as pg
 
 # Get user input
-def get_input():
+def get_input(inputs):
     # Keyboard input
     keys = pg.key.get_pressed()
     # Mouse input
     mousePos = pg.mouse.get_pos()
-    clicks = pg.mouse.get_pressed()
-    inputs = {"keys": keys, "mousePos": mousePos, "clicks": clicks}
+    mouseKeys = pg.mouse.get_pressed()
+    clicks = [0,0,0]
+    # Check if this is the first time the function is being called.
+    if inputs != None:
+        # Get mouse click impulse
+        clicks[0] = get_rising_edge(inputs, "mouseKeys", 0, mouseKeys[0])
+    inputs = {"keys": keys, "mousePos": mousePos, "mouseKeys": mouseKeys, "clicks": clicks}
     return inputs
 
 def player_quit():
@@ -20,3 +25,10 @@ def player_quit():
         if event.type == pg.QUIT or keys[pg.K_ESCAPE]:
             return False
     return True
+
+# Helper function for getting to get rising edge of a input.
+def get_rising_edge(inputs, type, index, subject):
+    if not inputs[type][index] and subject:
+        return 1
+    else:
+        return 0
